e335164b1d8cfc994551851ad651991c39543e6e
Katie <kaytlinrb@gmail.com>
2022-10-12 17:55:12 -0700

    add greeting and scanner to capture name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*** HELLO NEW CUSTOMER ***");
+        System.out.println("*** MAY WE PLEASE HAVE YOUR NAME? ***");
+        name = scanner.nextLine();
+        System.out.println("HELLO " + name.toUpperCase() + "! WELCOME TO GLAD RAGS!");
     }
 }
